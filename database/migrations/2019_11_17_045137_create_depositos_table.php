67b1a9c32a1bf4a27d9413b283b6332f39b4b813
Angel <angel.gomez.t89@gmail.com>
2019-11-16 23:48:52 -0600

    Inicio del Proyecto Larave  Please enter the commit message for your changes. Lines starting

---
+++ database/migrations/2019_11_17_045137_create_depositos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepositosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('depositos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Descripcion');
+            $table->string('Total');
+            $table->date('Fecha_Creacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('depositos');
+    }
+}
---
+++ database/migrations/2019_11_17_045841_create_cotizaciones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCotizacionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cotizaciones', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Folio');
+            $table->string('Cliente');
+            $table->string('Auto');
+            $table->string('Precio');
+            $table->date('Fecha_Creacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cotizaciones');
+    }
+}
